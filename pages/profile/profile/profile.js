eb47c954f449475a0f82edd6527473369f725400
lilang <7582190+lilanglilang@user.noreply.gitee.com>
2025-03-17 14:51:35 +0800

    updata

--- pages/profile/profile/profile.js
+++ pages/profile/profile/profile.js
@@ -1,18 +1,13 @@
 // miniprogram-1/pages/profile/profile/profile.js
 Page({
     data: {
-        userInfo: {},
-        genderList: ['男', '女'],
-        genderIndex: -1,
-        isCollapsed: false // 更多信息默认展开
+        userInfo: {}
     },
 
     onLoad() {
         // 从本地存储中读取用户信息
         const userInfo = wx.getStorageSync('userInfo') || {};
         this.setData({ userInfo });
-        const genderIndex = this.data.genderList.indexOf(this.data.userInfo.gender);
-        this.setData({ genderIndex });
     },
 
     onChooseAvatar(e) {
@@ -26,15 +21,6 @@ Page({
         const nickName = e.detail.value;
         getApp().globalData.userInfo.nickName = nickName;
         this.setData({ userInfo: getApp().globalData.userInfo });
-    },
-
-    onGenderChange(e) {
-        const gender = this.data.genderList[e.detail.value];
-        getApp().globalData.userInfo.gender = gender;
-        this.setData({ 
-            userInfo: getApp().globalData.userInfo,
-            genderIndex: e.detail.value
-        });
     },
 
     onBirthdayChange(e) {
@@ -55,27 +41,6 @@ Page({
         const introduction = e.detail.value;
         getApp().globalData.userInfo.introduction = introduction;
         this.setData({ userInfo: getApp().globalData.userInfo });
-    },
-
-    onChooseBackgroundImage() {
-        wx.chooseImage({
-            count: 1,
-            sizeType: ['original', 'compressed'],
-            sourceType: ['album', 'camera'],
-            success: res => {
-                const backgroundImage = res.tempFilePaths[0];
-                getApp().globalData.userInfo.backgroundImage = backgroundImage;
-                this.setData({ userInfo: getApp().globalData.userInfo });
-                getApp().saveUserInfoToStorage();
-            }
-        });
-    },
-
-    // 新增折叠切换方法
-    toggleCollapse() {
-        this.setData({
-            isCollapsed: !this.data.isCollapsed
-        });
     },
 
     // 新增确认按钮点击事件
